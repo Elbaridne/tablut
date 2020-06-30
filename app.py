114e78efa05bd631bb6f8b9ca13dfe92cc12c154
Elbaridne <mariobluemind@gmail.com>
2020-06-30 19:50:14 +0200

    frontend wip

--- app.py
+++ app.py
@@ -1,43 +1,63 @@
-from flask import Flask, render_template, session, url_for, redirect
+import json
+
+import requests
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit
+from flask_cors import CORS
+
 from tablut import Tafl
-from random import choice
-app = Flask(__name__)
-app.secret_key = "KEYULTRASECRETADELAMUERTEqueAUNQUELOINTENTENniPAATRASVANaPODERACABARDEScubr13nd0"
 
-@app.route("/")
-def tablut():
-    if 'jogando' in session:
-        gamestring = session['gamestring']
-        game = Tafl(fromjson=gamestring)        
+app = Flask(__name__, static_url_path='', static_folder='web/static')
+app.config['SECRET_KEY'] = 'some-super-secret-key'
+app.config['DEFAULT_PARSERS'] = [
+    'flask.ext.api.parsers.JSONParser',
+    'flask.ext.api.parsers.URLEncodedParser',
+    'flask.ext.api.parsers.FormParser',
+    'flask.ext.api.parsers.MultiPartParser'
+]
+app.debug = True
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+socketio = SocketIO(app)
+alive_objects = {}
 
-    else:
-        session['jogando'] = True
-        game = Tafl()
-        game.in_step(game.mask[-1])
-        session['gamestring'] = game.json()
+@app.route('/')
+def index():
+    return render_template('board.html')
 
-    moves = list(map(game.action_dec, game.mask))
-    print(moves)
-    return render_template('tablut.html', tablero=game.board, moves=moves)
 
-@app.route("/<move>", methods=['POST'])
-def tablut2(move):
-    if 'jogando' in session:
-        gamestring = session['gamestring']
-        game = Tafl(fromjson=gamestring)
-        if move in game.mask:
-            game.in_step(move)
-            # El agente hace un mov aleatorio
-            game.in_step(choice(game.mask))
-            moves = list(map(game.action_dec, game.mask))
-            return render_template('tablut.html', tablero=game.board, moves=moves, jugador=game.currentPlayer)
-        else:
-            pass
-            #Movimiento inválido
-        
+@socketio.on('connect', namespace="/tablut")
+def start_board():
+    game = Tafl()
+    g_json = game.json()
+    alive_objects[hex(id(game))] = game
+    emit('start_game', {'data': g_json})
 
-    else:
-        return redirect(url_for(tablut))
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@socketio.on('move', namespace='/tablut')
+def player_move(data):
+    
+    #board, stats, step = msg['state'], msg['stats'], msg['move']
+    game = Tafl(fromjson=json.dumps(data['state']))
+    game.in_step(int(data['movement']))
+    print(game)
+    emit('move', {'data':game.json()})
+
+
+# @app.after_request
+# def add_header(response):
+#     response.headers['Access-Control-Allow-Origin'] = '*'
+#     response.headers['Access-Control-Allow-Headers'] = 'Access-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, Authorization'
+#     response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS, HEAD'
+#     return response
+
+
+# @app.route('/', defaults={'path': ''})
+# @app.route('/<path:path>')
+# def catch_all(path):
+#     if app.debug:
+#         return requests.get('http://localhost:3000/{}'.format(path)).text
+#     return render_template("index.html")
+
+
+if __name__ == '__main__':
+    socketio.run(app)
